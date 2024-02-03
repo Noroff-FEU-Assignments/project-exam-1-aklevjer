331b9b6f45096dd99e2147ab4b9eb21fb3e6a4f0
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-02-03 15:46:59 +0100

    Simplified the alert message

--- js/ui/components/alert.js
+++ js/ui/components/alert.js
@@ -1,24 +1,11 @@
 import * as utils from "../../utils/index.js";
 
-function createErrorMessage(message) {
-  const errorIcon = utils.createHTMLElement("i", ["bx", "bx-error-circle", "bx-lg", "text-red"]);
-
-  const errorTitle = utils.createHTMLElement("strong", ["text-medium", "text-red"], "Error");
-  const errorBody = utils.createHTMLElement("p", null, message);
-  const errorContent = utils.createHTMLElement("div", null, null, [errorTitle, errorBody]);
-
-  return utils.createHTMLElement("div", ["error-message", "flex", "items-center", "gap-1", "rounded-corners"], null, [errorIcon, errorContent]);
-}
-
-function createSuccessMessage(message) {
-  const successIcon = utils.createHTMLElement("i", ["bx", "bx-check-circle"]);
-  const successBody = utils.createHTMLElement("p", null, message);
-
-  return utils.createHTMLElement("div", ["success-message", "flex", "items-center"], null, [successIcon, successBody]);
-}
-
 export function showAlertMessage(parentElement, alertType, message) {
-  const alertMessage = alertType === "success" ? createSuccessMessage(message) : createErrorMessage(message);
+  const isSuccessMsg = alertType === "success";
+
+  const alertIcon = utils.createHTMLElement("i", isSuccessMsg ? ["bx", "bx-check-circle"] : ["bx", "bx-error-circle", "text-red"]);
+  const alertBody = utils.createHTMLElement(isSuccessMsg ? "strong" : "p", null, message);
+  const alertMessage = utils.createHTMLElement("div", [`alert-${alertType}`, "flex", "items-center", "rounded-corners"], null, [alertIcon, alertBody]);
 
   if (parentElement) {
     parentElement.replaceWith(alertMessage);
