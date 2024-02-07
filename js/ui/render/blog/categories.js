c87edb623c374af5bc33e9cfdadb8dae64bf021a
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-02-07 19:36:08 +0100

    Minor refactoring

--- js/ui/render/blog/categories.js
+++ js/ui/render/blog/categories.js
@@ -1,7 +1,14 @@
 import * as utils from "../../../utils/index.js";
 
+function filterOutUncategorized(categories) {
+  return categories.filter((category) => {
+    const parsedCategory = utils.getParsedText(category.name);
+    return parsedCategory.toLowerCase() !== "uncategorized";
+  });
+}
+
 export function renderCategories(categories, categorySelect) {
-  const filteredCategories = categories.filter((category) => category.name.toLowerCase() !== "uncategorized");
+  const filteredCategories = filterOutUncategorized(categories);
 
   filteredCategories.forEach((category) => {
     const categoryOption = utils.createHTMLElement("option", null, utils.getParsedText(category.name));
