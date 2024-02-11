e89b0f16871108e9aa31e32e04ce8de230b8e863
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-02-11 15:56:46 +0100

    Small adjustment to the alert message

--- js/ui/components/alert.js
+++ js/ui/components/alert.js
@@ -1,14 +1,14 @@
 import * as utils from "../../utils/index.js";
 
-export function showAlertMessage(parentElement, alertType, message) {
+export function showAlertMessage(element, alertType, message) {
   const isSuccessMsg = alertType === "success";
 
   const alertIcon = utils.createHTMLElement("i", isSuccessMsg ? ["bx", "bx-check-circle"] : ["bx", "bx-error-circle", "text-red"]);
   const alertBody = utils.createHTMLElement(isSuccessMsg ? "strong" : "p", null, message);
   const alertMessage = utils.createHTMLElement("div", [`alert-${alertType}`, "flex", "items-center", "rounded-corners"], null, [alertIcon, alertBody]);
 
-  if (parentElement) {
-    parentElement.replaceWith(alertMessage);
+  if (element) {
+    element.replaceWith(alertMessage);
   }
 }
 
