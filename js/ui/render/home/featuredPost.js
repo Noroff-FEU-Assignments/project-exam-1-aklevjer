a8cda541523071c6d40d6834a08c56f9f88924d7
Aleksander Klevjer <alexz_91_@hotmail.com>
2024-02-12 12:59:04 +0100

    Improved BEM

--- js/ui/render/home/featuredPost.js
+++ js/ui/render/home/featuredPost.js
@@ -1,33 +1,33 @@
 import * as utils from "../../../utils/index.js";
 
-const featuredPostTemplate = `<img src="" alt="" class="featured-post__image rounded-corners" />
+const featuredPostTemplate = `<img src="" alt="" class="featured-post__article__image rounded-corners" />
                               <div class="flow">
-                                <h3 class="featured-post__title"></h3>
-                                <div class="featured-post__meta bullet-separator flex gap-0-5 text-sm">
+                                <h3 class="featured-post__article__title"></h3>
+                                <div class="featured-post__article__meta bullet-separator flex gap-0-5 text-sm">
                                   <div class="flex items-center gap-0-5">
-                                    <img src="" alt="" class="featured-post__author-avatar rounded-full" />
-                                    <span class="featured-post__author-name"></span>
+                                    <img src="" alt="" class="featured-post__article__author-avatar rounded-full" />
+                                    <span class="featured-post__article__author-name"></span>
                                   </div>
                                   <div class="flex items-center gap-0-5">
                                     <i class="bx bx-calendar bx-xs"></i>
-                                    <time datetime="" class="featured-post__date"></time>
+                                    <time datetime="" class="featured-post__article__date"></time>
                                   </div>
                                 </div>
-                                <p class="featured-post__body text-short truncate-text"></p>
-                                <a href="" class="featured-post__link btn btn-dark"></a>
+                                <p class="featured-post__article__body text-short truncate-text"></p>
+                                <a href="" class="featured-post__article__link btn btn-dark"></a>
                               </div>`;
 
 export function renderFeaturedPost(blogPost, featuredPostContainer) {
   const featuredPost = utils.createHTMLElement("article", ["featured-post__article", "grid", "place-items-center", "gap-2"]);
   featuredPost.innerHTML = featuredPostTemplate;
 
-  const featuredPostImage = featuredPost.querySelector(".featured-post__image");
-  const featuredPostTitle = featuredPost.querySelector(".featured-post__title");
-  const featuredPostAuthorAvatar = featuredPost.querySelector(".featured-post__author-avatar");
-  const featuredPostAuthorName = featuredPost.querySelector(".featured-post__author-name");
-  const featuredPostDate = featuredPost.querySelector(".featured-post__date");
-  const featuredPostBody = featuredPost.querySelector(".featured-post__body");
-  const featuredPostLink = featuredPost.querySelector(".featured-post__link");
+  const featuredPostImage = featuredPost.querySelector(".featured-post__article__image");
+  const featuredPostTitle = featuredPost.querySelector(".featured-post__article__title");
+  const featuredPostAuthorAvatar = featuredPost.querySelector(".featured-post__article__author-avatar");
+  const featuredPostAuthorName = featuredPost.querySelector(".featured-post__article__author-name");
+  const featuredPostDate = featuredPost.querySelector(".featured-post__article__date");
+  const featuredPostBody = featuredPost.querySelector(".featured-post__article__body");
+  const featuredPostLink = featuredPost.querySelector(".featured-post__article__link");
 
   // Image
   const blogPostImage = utils.getImageFromPost(blogPost);
